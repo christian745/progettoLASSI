0e5c5b3250b107ad5bae8ad3e8144cfaba49ee20
Lorenzo <lorenzo.capobianchi@gmail.com>
2020-07-11 15:31:48 +0200

    modifica user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_162827) do
+ActiveRecord::Schema.define(version: 2020_07_10_211822) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_07_09_162827) do
     t.boolean "admin", default: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
